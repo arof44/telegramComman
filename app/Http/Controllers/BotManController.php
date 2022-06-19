f2c33655f40ec80308fdd3724ff9e00f440918f8
aqil spc <aqil@intermediatech.id>
2022-06-19 22:12:53 +0700

    bot

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -6,7 +6,7 @@ use App\Conversations\BotTaniConversation;
 use BotMan\BotMan\Cache\LaravelCache;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
- 
+use DB;
  
 class BotManController extends Controller
 {
@@ -32,19 +32,59 @@ class BotManController extends Controller
             $bot->startConversation(new BotTaniConversation());
         })->stopsConversation();
  
-        // $botman->hears('/kitab|kitab', function (BotMan $bot) {
-        //     $bot->startConversation(new BotTaniConversation());
-        // })->stopsConversation();
- 
-        $botman->hears('/lapor|lapor|laporkan', function (BotMan $bot) {
-            $bot->reply('Silahkan laporkan di email aurof@gmail.com . Laporan akan segera di perbaiki , terimakasih.');
+        $botman->hears('/ringkasan|ringkasan', function (BotMan $bot) {
+            $data =  $this->getDataRingkasan();
+            $bot->reply($data);
         })->stopsConversation();
  
-        // $botman->hears('/tentang|about|tentang', function (BotMan $bot) {
-        //     $bot->reply('HaditsID Telegram Bot By ZaLabs. Mohon maaf jika server terasa lamban, dikarenakan menggunakan free hosting dari Heroku(.)com. Data didapatkan dari https://s.id/zXj6S .');
-        // })->stopsConversation();
+        $botman->hears('/restock|restock', function (BotMan $bot) {
+            $data =  $this->getDataReStock();
+            $bot->reply($data);
+        })->stopsConversation();
  
         $botman->listen();
+    }
+ 
+    public function getDataRingkasan()
+    {
+        $tersedia = DB::table('barang')->where('stock','>',5)->count();
+        $segera = DB::table('barang')->where('stock','<',5)->count();
+        $habis = DB::table('barang')->where('stock','=',0)->count();
+        $kata = 'Ringkasan Data Barang'."\n";
+       
+            $kata .= 'Stock Tersedia : '.$tersedia."\n";
+            $kata .= 'Stock  Segera Habis: '.$segera."\n";
+            $kata .= 'Stock Habis : '.$habis."\n";
+            $kata .= "\n".' Silahkan ketik /start lagi dan kirim untuk memulai ulang';
+        $rpl = str_replace(array("\n"), "\xA" , $kata);
+        $send = urlencode($rpl);
+        return $rpl;
+ 
+    }
+
+    public function getDataReStock()
+    {
+        $data = DB::table('barang')->where('stock','<',5)->get();
+        $kata = 'Detail Data Stock Barang'."\n";
+        
+        if(!$data->isEmpty())
+        {
+            foreach ($data as $key => $value) {
+                $kata .= $value->nama.' '.'Stock :'.$value->stock."\n";
+            }
+            $kata .= 'Total barang yang perlu di restock sebanyak'.' '.count($data)."\n".' Silahkan ketik /start lagi dan kirim untuk memulai ulang';
+        }else{
+            $all = DB::table('barang')->get();
+            $kata .= '\n Total barang : '.count($all)."\n";
+            foreach ($all as $key => $value) {
+                $kata .= $value->nama."\n".'Stock :'.$value->stock."\n";
+            }
+            $kata .= "\n".'Belum ada data barang yang perlu di restock ulang semua persediaan masih diatas 5 '."\n".' Silahkan ketik /start lagi dan kirim untuk memulai ulang';
+        }
+        $rpl = str_replace(array("\n"), "\xA" , $kata);
+        $send = urlencode($rpl);
+        return $rpl;
+ 
     }
  
 }
